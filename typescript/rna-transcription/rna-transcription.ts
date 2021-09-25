46e4cc042d56948359c320a8d7eea5fcba218b64
E-wave112 <osagieiyayi09@gmail.com>
2021-09-25 15:49:14 +0000

    updated the readme file

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,33 +1,33 @@
-export function toRna(str:string):string {
+export function toRna(str: string): string {
   //create an rna to dna map object
   interface rnaObjectKeys {
     [key: string]: string;
   }
 
-interface rnaObj extends rnaObjectKeys {
-  G:string;
-  C:string;
-  T:string;
-  A:string;
-}
+  interface rnaObj extends rnaObjectKeys {
+    G: string;
+    C: string;
+    T: string;
+    A: string;
+  }
 
-let rnaMap: rnaObj =  {
+  let rnaMap: rnaObj = {
     G: 'C',
     C: 'G',
     T: 'A',
     A: 'U'
   };
-  if (str.length ===1 && rnaMap[str]) {
+  if (str.length === 1 && rnaMap[str]) {
     return rnaMap[str]
     //transcribe the dna to rna
-  } else if (str.length === 1 && !rnaMap[str]){
+  } else if (str.length === 1 && !rnaMap[str]) {
     throw new Error("Invalid input DNA.");
-  } else if (str.length > 1 && str.length % 4 !== 0){
+  } else if (str.length > 1 && str.length % 4 !== 0) {
     throw new Error("Invalid input DNA.");
   } else {
-    let rna:string = '';
-    for (let i = 0; i < str.length; i++){
-      if (!(rnaMap[str[i]])){
+    let rna: string = '';
+    for (let i = 0; i < str.length; i++) {
+      if (!(rnaMap[str[i]])) {
         throw new Error("Invalid input DNA.");
       }
       rna += rnaMap[str[i]];
